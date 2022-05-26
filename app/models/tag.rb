931dae777a3b6b82987371dcebdb142f8afc2130
Brayan Diaz C <brayandiazc@gmail.com>
2022-05-26 18:49:55 -0500

    feat: Relation great

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,6 @@
 class Tag < ApplicationRecord
   #Relationships
-  has_and_belongs_to_many :posts
+  # has_and_belongs_to_many :posts
+  has_many :post_tags
+  has_many :posts, through: :post_tags
 end
