b6c767a55ee798dc1155f56b7d91abffc84e6f6f
Brayan Diaz C <brayandiazc@gmail.com>
2022-05-26 19:38:26 -0500

    feat: Depende destroy added

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,5 @@ class Post < ApplicationRecord
   #Relationships
   # has_and_belongs_to_many :tags
   has_many :post_tags
-  has_many :tags, through: :post_tags
+  has_many :tags, through: :post_tags, dependent: destroy
 end
